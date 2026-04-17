6f987c41ce0c277d4acb76e7ade42df39eb581a6
Z-Link-C <zachery.croft@gmail.com>
2026-04-17 03:57:45 -0400

    Display Feature Complete

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toyData}) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{/* Render the collection of ToyCards */}
+      {toyData.map(toy=><ToyCard key={toy.id}{...toy}/>)}
+    </div>
   );
 }
 
